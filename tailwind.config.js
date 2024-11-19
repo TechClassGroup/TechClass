a82554f386fb89bb9ef72f165ccd0f8838203104
erduotong <3135351980@qq.com>
2024-11-19 22:14:28 +0800

    项目依赖配置完成

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import("tailwindcss").Config} */
 export default {
-    purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     content: [
         "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
+        "./src/*.{js,ts,jsx,tsx,vue}",
     ],
     theme: {
         extend: {},
