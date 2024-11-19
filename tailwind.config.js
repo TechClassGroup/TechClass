6fa3d6bd7e0f86c13fcb30dc941354d3e318e469
erduotong <3135351980@qq.com>
2024-11-19 22:09:27 +0800

    something update

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import("tailwindcss").Config} */
+export default {
+    purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
+
