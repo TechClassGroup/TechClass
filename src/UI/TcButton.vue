a1aaa11ad20867e00a7b82a00268df18ca065d30
erduotong <3135351980@qq.com>
2024-11-20 20:35:16 +0800

    CSS INIT

---
+++ src/UI/TcButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  disabled: boolean
+}
+
+const {disabled = false} = defineProps<Props>()
+</script>
+
+<template>
+  <button :class="disabled? 'absolute' : 'break-words'" >
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+
+</style>
