b948c89bc53fbcc05dadd4001f37819d8dde605f
erduotong <3135351980@qq.com>
2024-12-13 21:10:41 +0800

    后端

---
+++ src-tauri/src/error.rs
@@ -0,0 +1,31 @@
+/// 一些错误
+
+#[derive(Debug, thiserror::Error)]
+pub enum IpcError {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error(transparent)]
+    Json(#[from] serde_json::Error),
+}
+
+#[derive(serde::Serialize)]
+#[serde(tag = "kind", content = "message")]
+#[serde(rename_all = "camelCase")]
+pub enum IpcErrorKind {
+    Io(String),
+    Json(String),
+}
+
+impl serde::Serialize for IpcError {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::ser::Serializer,
+    {
+        let error_message = self.to_string();
+        let error_kind = match self {
+            Self::Io(_) => IpcErrorKind::Io(error_message),
+            Self::Json(_) => IpcErrorKind::Json(error_message),
+        };
+        error_kind.serialize(serializer)
+    }
+}
---
+++ src-tauri/src/storage.rs
@@ -0,0 +1,76 @@
+//! 存储管理
+
+use std::fs::OpenOptions;
+use std::io::{Read, Write};
+use std::path::PathBuf;
+
+use crate::{contestants::PATH_BASIC, error::IpcError};
+use lazy_static::lazy_static;
+use log::{info, warn};
+use serde_json::Value;
+
+lazy_static! {
+    pub static ref PATH_CONFIG: PathBuf = PATH_BASIC.join("config");
+}
+const CONFIG_SUFFIX: &str = "config.json";
+
+/// 获取存储路径
+fn get_path(id: &str) -> PathBuf {
+    let file_name = format!("{id}.{CONFIG_SUFFIX}");
+    PATH_CONFIG.join(&file_name)
+}
+
+#[tauri::command]
+/// 写入内容到[id]
+pub async fn storage_content(id: String, content: Value) -> Result<(), IpcError> {
+    let path = get_path(&id);
+    let stringified_content = match serde_json::to_vec(&content) {
+        Ok(content) => content,
+        Err(e) => {
+            warn!("转字符串出错 {}", e);
+            return Err(IpcError::Json(e));
+        }
+    };
+    let mut file = match OpenOptions::new().write(true).create(true).open(&path) {
+        Ok(file) => file,
+        Err(e) => {
+            warn!("打开文件时出错: {}", e);
+            return Err(IpcError::Io(e));
+        }
+    };
+
+    if let Err(e) = file.write_all(&stringified_content) {
+        warn!("Failed to write to file: {}", e);
+        return Err(IpcError::Io(e));
+    }
+    log::trace!("成功为 [{id}] 存储内容");
+
+    Ok(())
+}
+#[tauri::command]
+/// 把存储着[id]的内容读取出来
+pub fn load_content(id: String) -> Result<Value, IpcError> {
+    let path = get_path(&id);
+    let mut file = match OpenOptions::new().read(true).create(true).open(&path) {
+        Ok(file) => file,
+        Err(e) => {
+            warn!("打开文件时出错: {}", e);
+            return Err(IpcError::Io(e));
+        }
+    };
+    let mut content = String::new();
+    if let Err(e) = file.read_to_string(&mut content) {
+        warn!("读取文件内容时出错: {}", e);
+        return Err(IpcError::Io(e));
+    }
+
+    let json_content: Value = match serde_json::from_str(&content) {
+        Ok(content) => content,
+        Err(e) => {
+            warn!("解析 JSON 内容时出错: {}", e);
+            return Err(IpcError::Json(e));
+        }
+    };
+
+    Ok(json_content)
+}
