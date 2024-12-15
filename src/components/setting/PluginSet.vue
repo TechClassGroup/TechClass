168774ba2dc0fd2e025e84b50a97da4f02fdbb97
erduotong <3135351980@qq.com>
2024-12-15 10:43:17 +0800

    删除一个没啥用依赖

--- src/components/setting/PluginSet.vue
+++ src/components/setting/PluginSet.vue
@@ -3,7 +3,6 @@ import {useApplicationStore} from "@/stores/useApplicationStore.ts";
 import {official_plugins} from "@/plugins/official_plugins.ts";
 import TcSwitch from "@/UI/TcSwitch.vue";
 import TcButton from "@/UI/TcButton.vue";
-import {ref} from "vue";
 import {init_plugins} from "@/modules/plugins_manager.ts";
 // 数据流动: 按钮更新 --> store变化 --> 按钮状态变化
 const store = useApplicationStore()
