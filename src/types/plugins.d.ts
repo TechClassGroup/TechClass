b40ef68a921f691722ce3cbd80b815b164db829c
erduotong <3135351980@qq.com>
2025-01-01 17:11:19 +0800

    fix: 加载的时候不需要类型

--- src/types/plugins.d.ts
+++ src/types/plugins.d.ts
@@ -1,42 +1,41 @@
 /**
  * @fileOverview 插件的类型
  */
-import {defineComponent} from "vue";
-import {Store} from "pinia";
+import { defineComponent } from "vue";
+import { Store } from "pinia";
 
 /**
  * 插件的组件
  */
-interface PluginComponent<MainComponentKey extends string> {
-    setting_page: ReturnType<typeof defineComponent> | null;
-    main_page: Record<MainComponentKey, ReturnType<typeof defineComponent>> | null;
-
+interface PluginComponent<T extends string = string> {
+  setting_page: ReturnType<typeof defineComponent> | null;
+  main_page: Record<T, ReturnType<typeof defineComponent>> | null;
 }
 
 /**
  * 插件对象
  */
-export interface IPlugin<MainComponentKey extends string> {
-    name: string;
-    id: string;
-    description: string;
-    component: PluginComponent<MainComponentKey>;
-    init: (app: Store) => void
+export interface IPlugin<T extends string = string> {
+  name: string;
+  id: string;
+  description: string;
+  component: PluginComponent<T>;
+  init: (app: Store) => void;
 }
 
 /**
  * 插件实例
  */
 export interface InstancePlugin {
-    pluginObject: IPlugin
-    store: Store;
+  pluginObject: IPlugin;
+  store: Store;
 }
 
 /**
  * 仅暴露插件的组件以及其store
  */
-export interface PluginComponentStore<MainComponentKey extends string> {
-    component: PluginComponent<MainComponentKey>;
-    store: Store;
-    id: string;
+export interface PluginComponentStore {
+  component: PluginComponent;
+  store: Store;
+  id: string;
 }
