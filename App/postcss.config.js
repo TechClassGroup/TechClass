229e1d3e3dcfec54324ec4213a5a17469d939828
erduotong <3135351980@qq.com>
2025-01-27 17:04:15 +0800

    build: 添加autoprefixer和tailwindcss nesting

--- App/postcss.config.js
+++ App/postcss.config.js
@@ -1,6 +1,10 @@
 export default {
     plugins: {
+        "tailwindcss/nesting": {},
         tailwindcss: {},
-        autoprefixer: {},
+        autoprefixer: {
+            flexbox: "no-2009",
+            grid: true,
+        },
     },
 };
