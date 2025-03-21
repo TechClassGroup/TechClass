61b9c3f088d32ff0c07fe6fb17f1f882c2f5d93a
erduotong <3135351980@qq.com>
2025-03-21 22:40:29 +0800

    feat(ui): 主题色

--- App/tailwind.config.js
+++ App/tailwind.config.js
@@ -9,17 +9,11 @@ export default {
         extend: {
             colors: {
                 primary: {
-                    50: "rgb(var(--color-primary-50))",
                     100: "rgb(var(--color-primary-100))",
-                    200: "rgb(var(--color-primary-200))",
                     300: "rgb(var(--color-primary-300))",
-                    400: "rgb(var(--color-primary-400))",
                     500: "rgb(var(--color-primary-500))",
-                    600: "rgb(var(--color-primary-600))",
                     700: "rgb(var(--color-primary-700))",
-                    800: "rgb(var(--color-primary-800))",
                     900: "rgb(var(--color-primary-900))",
-                    950: "rgb(var(--color-primary-950))",
                     DEFAULT: "rgb(var(--color-primary-500))",
                 },
             },
