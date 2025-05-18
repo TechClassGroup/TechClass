22dd5ff6ab89dda4e07b1e71d24b33af0658a785
erduotong <3135351980@qq.com>
2025-05-18 22:55:05 +0800

    fix: 更新 vue-draggable-resizable 的模块声明

--- App/src/types/vue-draggable-resizable.d.ts
+++ App/src/types/vue-draggable-resizable.d.ts
@@ -1,7 +1,7 @@
 /**
  * @fileoverview 声明 vue-draggable-resizable 组件 防止 ts 报错
  */
-declare module "./vue-draggable-resizable" {
+declare module "vue-draggable-resizable" {
     const content: any;
     export default content;
 }
